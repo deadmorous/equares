e98d3ebcbeaceb1bb487febf0287a06526545504
Stepan Orlov <majorsteve@mail.ru>
2015-05-19 19:51:32 +0400

    Added copyright notice to fsmx.h

--- equares_http/public/include/fsmx.h
+++ equares_http/public/include/fsmx.h
@@ -1,3 +1,13 @@
+/*
+EquaRes - a tool for numerical analysis of equations.
+Copyright (C) 2015 Stepan Orlov
+
+This file is part of EquaRes.
+
+Full text of copyright notice can be found in file copyright_notice.txt in the EquaRes root directory.
+License agreement can be found in file LICENSE.md in the EquaRes root directory.
+*/
+
 #ifndef FSMX_H
 #define FSMX_H
 
